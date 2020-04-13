98b786daa228ec2264bd8021824ede561b281572
karaname <rekhov.ka@gmail.com>
2020-04-13 22:42:59 +0300

    detab / formatting

--- src/main.c
+++ src/main.c
@@ -5,50 +5,50 @@
 static void
 make_buttons_grid()
 {
-	gtk_grid_insert_row(GTK_GRID(gr.grid_buttns), 1);
-	gtk_grid_insert_column(GTK_GRID(gr.grid_buttns), 4);
-	gtk_grid_set_column_spacing(GTK_GRID(gr.grid_buttns), 40);
-	gtk_grid_set_column_homogeneous(GTK_GRID(gr.grid_buttns), TRUE);
+  gtk_grid_insert_row(GTK_GRID(gr.grid_buttns), 1);
+  gtk_grid_insert_column(GTK_GRID(gr.grid_buttns), 4);
+  gtk_grid_set_column_spacing(GTK_GRID(gr.grid_buttns), 40);
+  gtk_grid_set_column_homogeneous(GTK_GRID(gr.grid_buttns), TRUE);
   gtk_widget_set_valign(GTK_WIDGET(gr.grid_buttns), GTK_ALIGN_CENTER);
 }
 
 static void
 make_initial_grid()
 {
-	gtk_grid_insert_row(GTK_GRID(gr.grid_main), 3);
-	gtk_grid_insert_column(GTK_GRID(gr.grid_main), 2);
-	gtk_grid_set_row_spacing(GTK_GRID(gr.grid_main), 20);
-	gtk_grid_set_column_spacing(GTK_GRID(gr.grid_main), 20);
-	gtk_grid_set_row_homogeneous(GTK_GRID(gr.grid_main), TRUE);
-	gtk_grid_set_column_homogeneous(GTK_GRID(gr.grid_main), TRUE);
-	gtk_container_set_border_width(GTK_CONTAINER(gr.grid_main), 20);
+  gtk_grid_insert_row(GTK_GRID(gr.grid_main), 3);
+  gtk_grid_insert_column(GTK_GRID(gr.grid_main), 2);
+  gtk_grid_set_row_spacing(GTK_GRID(gr.grid_main), 20);
+  gtk_grid_set_column_spacing(GTK_GRID(gr.grid_main), 20);
+  gtk_grid_set_row_homogeneous(GTK_GRID(gr.grid_main), TRUE);
+  gtk_grid_set_column_homogeneous(GTK_GRID(gr.grid_main), TRUE);
+  gtk_container_set_border_width(GTK_CONTAINER(gr.grid_main), 20);
 }
 
 static void
 make_tree_view()
 {
-	gtk_tree_view_set_search_column(GTK_TREE_VIEW(tv.tree_view), -1);
-	gtk_tree_view_set_show_expanders(GTK_TREE_VIEW(tv.tree_view), FALSE);
-	gtk_tree_view_set_headers_clickable(GTK_TREE_VIEW(tv.tree_view), FALSE);
+  gtk_tree_view_set_search_column(GTK_TREE_VIEW(tv.tree_view), -1);
+  gtk_tree_view_set_show_expanders(GTK_TREE_VIEW(tv.tree_view), FALSE);
+  gtk_tree_view_set_headers_clickable(GTK_TREE_VIEW(tv.tree_view), FALSE);
 
   tv.selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(tv.tree_view));
-	gtk_tree_selection_set_mode(GTK_TREE_SELECTION(tv.selection), GTK_SELECTION_NONE);
+  gtk_tree_selection_set_mode(GTK_TREE_SELECTION(tv.selection), GTK_SELECTION_NONE);
 
-	gtk_list_store_append(GTK_LIST_STORE(tv.list_store), &tv.iter);
+  gtk_list_store_append(GTK_LIST_STORE(tv.list_store), &tv.iter);
 
   gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_TOTAL, "Total",
      tv.renderer, "text", COLUMN_TOTAL, NULL);
-	gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_LETTER, "Letter",
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_LETTER, "Letter",
     tv.renderer, "text", COLUMN_LETTER, NULL);
-	gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_SPECIAL, "Special",
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_SPECIAL, "Special",
     tv.renderer, "text", COLUMN_SPECIAL, NULL);
-	gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_SPACE, "Space",
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_SPACE, "Space",
     tv.renderer, "text", COLUMN_SPACE, NULL);
-	gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_NUM, "Number",
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_NUM, "Number",
     tv.renderer, "text", COLUMN_NUM, NULL);
-	gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_EVEN, "Even",
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_EVEN, "Even",
     tv.renderer, "text", COLUMN_EVEN, NULL);
-	gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_ODD, "Odd",
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(tv.tree_view), COLUMN_ODD, "Odd",
     tv.renderer, "text", COLUMN_ODD, NULL);
 }
 
@@ -57,61 +57,61 @@ make_scrolled_window(GtkWidget *scrolled_window,
 GtkTextBuffer *text_buffer, GtkWidget *text_view,
 gboolean value)
 {
-	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
     GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
 
   if (value == 1)
-	  gtk_text_buffer_set_text(text_buffer,
+    gtk_text_buffer_set_text(text_buffer,
       "Enter the text and click on any button you like ...", -1);
 
-	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
-	gtk_text_view_set_monospace(GTK_TEXT_VIEW(text_view), TRUE);
+  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
+  gtk_text_view_set_monospace(GTK_TEXT_VIEW(text_view), TRUE);
 
   /* MARGINS */
-	gtk_text_view_set_left_margin(GTK_TEXT_VIEW(text_view), 5);
-	gtk_text_view_set_right_margin(GTK_TEXT_VIEW(text_view), 8);
-	gtk_text_view_set_top_margin(GTK_TEXT_VIEW(text_view), 5);
-	gtk_text_view_set_bottom_margin(GTK_TEXT_VIEW(text_view), 5);
+  gtk_text_view_set_left_margin(GTK_TEXT_VIEW(text_view), 5);
+  gtk_text_view_set_right_margin(GTK_TEXT_VIEW(text_view), 8);
+  gtk_text_view_set_top_margin(GTK_TEXT_VIEW(text_view), 5);
+  gtk_text_view_set_bottom_margin(GTK_TEXT_VIEW(text_view), 5);
 
   /* PIXELS */
-	gtk_text_view_set_pixels_above_lines(GTK_TEXT_VIEW(text_view), 3);
-	gtk_text_view_set_pixels_below_lines(GTK_TEXT_VIEW(text_view), 3);
-	gtk_text_view_set_pixels_inside_wrap(GTK_TEXT_VIEW(text_view), 2);
+  gtk_text_view_set_pixels_above_lines(GTK_TEXT_VIEW(text_view), 3);
+  gtk_text_view_set_pixels_below_lines(GTK_TEXT_VIEW(text_view), 3);
+  gtk_text_view_set_pixels_inside_wrap(GTK_TEXT_VIEW(text_view), 2);
 
-	gtk_container_add(GTK_CONTAINER(scrolled_window), text_view);
+  gtk_container_add(GTK_CONTAINER(scrolled_window), text_view);
 }
 
 static GtkWidget *
 make_common()
 {
-	gr.grid_main = gtk_grid_new();
+  gr.grid_main = gtk_grid_new();
   gr.grid_buttns = gtk_grid_new();
 
   /* -- FIRST ROW WIDGETS */
   /* scrolled window */
-	frw.scrolled_window_FR = gtk_scrolled_window_new(NULL, NULL);
-	bf.text_buffer_FR = gtk_text_buffer_new(NULL);
-	frw.text_view_FR = gtk_text_view_new_with_buffer(bf.text_buffer_FR);
+  frw.scrolled_window_FR = gtk_scrolled_window_new(NULL, NULL);
+  bf.text_buffer_FR = gtk_text_buffer_new(NULL);
+  frw.text_view_FR = gtk_text_view_new_with_buffer(bf.text_buffer_FR);
   make_scrolled_window(frw.scrolled_window_FR, bf.text_buffer_FR, frw.text_view_FR, 1);
 
   /* clear button */
   bt.clear_buttn = gtk_button_new_with_label("Clear");
-	gtk_container_set_border_width(GTK_CONTAINER(bt.clear_buttn), 60);
+  gtk_container_set_border_width(GTK_CONTAINER(bt.clear_buttn), 60);
 
   /* -- SECOND ROW WIDGETS */
   /* tree view */
-	tv.list_store = gtk_list_store_new(N_COLUMNS, G_TYPE_INT,
-		G_TYPE_INT, G_TYPE_INT, G_TYPE_INT,
-		G_TYPE_INT, G_TYPE_INT, G_TYPE_INT);
+  tv.list_store = gtk_list_store_new(N_COLUMNS, G_TYPE_INT,
+    G_TYPE_INT, G_TYPE_INT, G_TYPE_INT,
+    G_TYPE_INT, G_TYPE_INT, G_TYPE_INT);
 
-	tv.tree_view = gtk_tree_view_new_with_model(GTK_TREE_MODEL(tv.list_store));
-	tv.renderer = gtk_cell_renderer_text_new();
+  tv.tree_view = gtk_tree_view_new_with_model(GTK_TREE_MODEL(tv.list_store));
+  tv.renderer = gtk_cell_renderer_text_new();
   make_tree_view();
 
   /* scrolled window */
-	srw.scrolled_window_SR = gtk_scrolled_window_new(NULL, NULL);
-	bf.text_buffer_SR = gtk_text_buffer_new(NULL);
-	srw.text_view_SR = gtk_text_view_new_with_buffer(bf.text_buffer_SR);
+  srw.scrolled_window_SR = gtk_scrolled_window_new(NULL, NULL);
+  bf.text_buffer_SR = gtk_text_buffer_new(NULL);
+  srw.text_view_SR = gtk_text_view_new_with_buffer(bf.text_buffer_SR);
   make_scrolled_window(srw.scrolled_window_SR, bf.text_buffer_SR, srw.text_view_SR, 0);
 
   /* -- THIRD ROW WIDGETS */
@@ -125,18 +125,18 @@ make_common()
   make_initial_grid();
 
   /* ATTACH WIDGETS TO GRID_MAIN */
-	gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(frw.scrolled_window_FR), 0, 0, 1, 1);
-	gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(bt.clear_buttn), 1, 0, 1, 1);
-	gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(tv.tree_view), 0, 1, 1, 1);
-	gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(srw.scrolled_window_SR), 1, 1, 1, 1);
-	gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(gr.grid_buttns), 0, 2, 2, 1);
+  gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(frw.scrolled_window_FR), 0, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(bt.clear_buttn), 1, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(tv.tree_view), 0, 1, 1, 1);
+  gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(srw.scrolled_window_SR), 1, 1, 1, 1);
+  gtk_grid_attach(GTK_GRID(gr.grid_main), GTK_WIDGET(gr.grid_buttns), 0, 2, 2, 1);
   /* left, top, width, height */
 
   /* ATTACH BUTTONS TO GRID_BUTTNS */
-	gtk_grid_attach(GTK_GRID(gr.grid_buttns), GTK_WIDGET(bt.lower_buttn), 0, 0, 1, 1);
-	gtk_grid_attach(GTK_GRID(gr.grid_buttns), GTK_WIDGET(bt.upper_buttn), 1, 0, 1, 1);
-	gtk_grid_attach(GTK_GRID(gr.grid_buttns), GTK_WIDGET(bt.title_buttn), 2, 0, 1, 1);
-	gtk_grid_attach(GTK_GRID(gr.grid_buttns), GTK_WIDGET(bt.rev_buttn), 3, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(gr.grid_buttns), GTK_WIDGET(bt.lower_buttn), 0, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(gr.grid_buttns), GTK_WIDGET(bt.upper_buttn), 1, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(gr.grid_buttns), GTK_WIDGET(bt.title_buttn), 2, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(gr.grid_buttns), GTK_WIDGET(bt.rev_buttn), 3, 0, 1, 1);
 
   return GTK_WIDGET (gr.grid_main);
 }
@@ -147,22 +147,22 @@ int main(int argc, char *argv[])
 
   gtk_init(&argc, &argv);
 
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window), "Register Converter");
-	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ALWAYS);
-	gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Register Converter");
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ALWAYS);
+  gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
   gtk_window_maximize(GTK_WINDOW(window));
 
-	gtk_container_add(GTK_CONTAINER(window), make_common());
-	gtk_widget_show_all(window);
+  gtk_container_add(GTK_CONTAINER(window), make_common());
+  gtk_widget_show_all(window);
 
-	g_signal_connect(G_OBJECT(window), "destroy", gtk_main_quit, NULL);
-	g_signal_connect(G_OBJECT(bf.text_buffer_FR), "insert-text", G_CALLBACK(max_buffer_insert), NULL);
-	g_signal_connect(G_OBJECT(bt.lower_buttn), "clicked", G_CALLBACK(on_lower_button_clicked), NULL);
-	g_signal_connect(G_OBJECT(bt.upper_buttn), "clicked", G_CALLBACK(on_upper_button_clicked), NULL);
-	g_signal_connect(G_OBJECT(bt.title_buttn), "clicked", G_CALLBACK(on_title_button_clicked), NULL);
-	g_signal_connect(G_OBJECT(bt.rev_buttn), "clicked", G_CALLBACK(on_reverse_button_clicked), NULL);
-	g_signal_connect(G_OBJECT(bt.clear_buttn), "clicked", G_CALLBACK(on_clear_button_clicked), NULL);
+  g_signal_connect(G_OBJECT(window), "destroy", gtk_main_quit, NULL);
+  g_signal_connect(G_OBJECT(bf.text_buffer_FR), "insert-text", G_CALLBACK(max_buffer_insert), NULL);
+  g_signal_connect(G_OBJECT(bt.lower_buttn), "clicked", G_CALLBACK(on_lower_button_clicked), NULL);
+  g_signal_connect(G_OBJECT(bt.upper_buttn), "clicked", G_CALLBACK(on_upper_button_clicked), NULL);
+  g_signal_connect(G_OBJECT(bt.title_buttn), "clicked", G_CALLBACK(on_title_button_clicked), NULL);
+  g_signal_connect(G_OBJECT(bt.rev_buttn), "clicked", G_CALLBACK(on_reverse_button_clicked), NULL);
+  g_signal_connect(G_OBJECT(bt.clear_buttn), "clicked", G_CALLBACK(on_clear_button_clicked), NULL);
 
   gtk_main();
 
--- src/main.h
+++ src/main.h
@@ -10,37 +10,37 @@ enum {
 };
 
 struct Buttons {
-	GtkWidget *lower_buttn;
-	GtkWidget *upper_buttn;
-	GtkWidget *title_buttn;
-	GtkWidget *rev_buttn;
-	GtkWidget *clear_buttn;
+  GtkWidget *lower_buttn;
+  GtkWidget *upper_buttn;
+  GtkWidget *title_buttn;
+  GtkWidget *rev_buttn;
+  GtkWidget *clear_buttn;
 } bt;
 
 struct TreeView {
-	GtkListStore *list_store;
-	GtkWidget *tree_view;
-	GtkTreeSelection *selection;
-	GtkTreeIter iter;
-	GtkCellRenderer *renderer;
+  GtkListStore *list_store;
+  GtkWidget *tree_view;
+  GtkTreeSelection *selection;
+  GtkTreeIter iter;
+  GtkCellRenderer *renderer;
 } tv;
 
 struct SecondRowWindow {
-	GtkWidget *scrolled_window_SR;
-	GtkWidget *text_view_SR;
+  GtkWidget *scrolled_window_SR;
+  GtkWidget *text_view_SR;
 } srw;
 
 struct FirstRowWindow {
-	GtkWidget *scrolled_window_FR;
-	GtkWidget *text_view_FR;
+  GtkWidget *scrolled_window_FR;
+  GtkWidget *text_view_FR;
 } frw;
 
 struct Buffers {
-	GtkTextBuffer *text_buffer_FR;
-	GtkTextBuffer *text_buffer_SR;
+  GtkTextBuffer *text_buffer_FR;
+  GtkTextBuffer *text_buffer_SR;
 } bf;
 
 struct Grids {
-	GtkWidget *grid_main;
-	GtkWidget *grid_buttns;
+  GtkWidget *grid_main;
+  GtkWidget *grid_buttns;
 } gr;
